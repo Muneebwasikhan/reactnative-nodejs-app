78580dd9006a1dfb461341f4f3c4b813565aafcd
NajamShehzad <najambutt195@gmail.com>
2019-02-22 17:31:11 +0500

    File Upload

--- NodeExpressBoilerplate/routes/addService.js
+++ NodeExpressBoilerplate/routes/addService.js
@@ -1,36 +1,38 @@
-exports = module.exports = function(app, mongoose) {
+exports = module.exports = function (app, mongoose) {
   var express = require("express");
   var router = express.Router();
   const multer = require("multer");
   const cloudinary = require("cloudinary");
   const validator = require("validator");
+
+
   var storage = multer.diskStorage({
     filename: function (req, file, callback) {
-        callback(null, Date.now() + file.originalname);
+      callback(null, Date.now() + file.originalname);
     }
-});
-var imageFilter = function (req, file, cb) {
+  });
+  var imageFilter = function (req, file, cb) {
     // accept image files only
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
 
-        req.imgError = true;
-        // return cb(new Error("Only image files are allowed!"), true);
+      req.imgError = true;
+      // return cb(new Error("Only image files are allowed!"), true);
     }
     cb(null, true);
-};
-var upload = multer({ storage: storage, fileFilter: imageFilter });
+  };
+  var upload = multer({ storage: storage, fileFilter: imageFilter });
 
-cloudinary.config({
+  cloudinary.config({
     cloud_name: app.get('cloud_name'),
     api_key: app.get('api_key'),
     api_secret: app.get('api_secret')
-});
+  });
 
   /* GET users listing. */
-  router.post("/", upload.single("image") , async (req, res, next) => {
+  router.post("/", upload.single('image'), async (req, res, next) => {
     // console.log(req.body)
     try {
-      console.log(req.file)
+      console.log(req.file);
       res.send('data')
     } catch (error) {
       res.send({ success: false, message: error.message });

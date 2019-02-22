9b481912aea505140dbd1c7cb634665d9dc183c6
NajamShehzad <najambutt195@gmail.com>
2019-02-23 03:19:49 +0500

    chat model

---
+++ NodeExpressBoilerplate/model/chat.js
@@ -0,0 +1,28 @@
+exports = module.exports = function (app, mongoose) {
+    "use strict"
+    let Schema = mongoose.Schema
+
+    let UserSchema = new Schema({
+        person1_id: {
+            type: String,
+            required:true
+        },
+        person1_name: {
+            type: String,
+            required:true
+        },
+        person2_id: {
+            type: String,
+            required:true
+        },
+        person2_name: {
+            type: String,
+            required:true
+        },
+        chatInitDate: {
+            type: Number,
+            default: Date.now()
+        }
+    })
+    app.db.model("Chat", UserSchema)
+}

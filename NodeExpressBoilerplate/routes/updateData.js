b84fdbaee9ce69922c28bcbd1872195b7fd524a5
NajamShehzad <najambutt195@gmail.com>
2019-02-23 08:43:20 +0500

    Upload Image on Cloudnary

--- NodeExpressBoilerplate/routes/updateData.js
+++ NodeExpressBoilerplate/routes/updateData.js
@@ -1,16 +1,45 @@
-exports = module.exports = function(app, mongoose) {
+exports = module.exports = function (app, mongoose) {
   var express = require("express")
-  var router = express.Router()
+  var router = express.Router();
+
+
+  const cloudinary = require("cloudinary")
+  const validator = require("validator")
+  // const imgUpload  =require('./imgUploadMiddleWare');
+
+  cloudinary.config({
+    cloud_name: app.get("cloud_name"),
+    api_key: app.get("api_key"),
+    api_secret: app.get("api_secret")
+  });
+
+
 
   /* GET users listing. */
   router.post("/numberandprofile", async (req, res, next) => {
     // console.log(req.body)
     try {
-      let upadataDataNumImg = await app.db.models.RegStudent.findOneAndUpdate(
-        { fbId: req.body.fbId },
-        { $set: { phoneNumber: req.body.phoneNumber, profilePhoto: req.body.profilePhoto } },
-        { new: true }
-      )
+
+      if (!req.body.profilePhoto) {
+        res.send({
+          success: false,
+          message: "Please provide Profile Image"
+        })
+      }
+
+      let profileImageObj = await uploadImage(req.body.profilePhoto);
+
+
+      let upadataDataNumImg = await app.db.models.RegStudent.findOneAndUpdate({
+        fbId: req.body.fbId
+      }, {
+        $set: {
+          phoneNumber: req.body.phoneNumber,
+          profilePhoto: profileImageObj.secure_url
+        }
+      }, {
+        new: true
+      })
       if (upadataDataNumImg) {
         return res.send({
           success: true,
@@ -23,9 +52,41 @@ exports = module.exports = function(app, mongoose) {
         })
       }
     } catch (error) {
-      res.send({ success: false, message: error.message })
+      res.send({
+        success: false,
+        message: error.message
+      })
     }
   })
 
-  app.use("/updateData", router)
+  app.use("/updateData", router);
+
+
+
+
+  //Upload Image
+  async function uploadImage(image) {
+    return new Promise((resolve, reject) => {
+      try {
+        cloudinary.v2.uploader.upload(
+          image, {
+            secure: true
+          },
+          (err, imgData) => {
+            if (err) {
+              reject(err)
+            }
+            console.log(imgData.secure_url)
+            resolve(imgData)
+          }
+        )
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+
+
+
 }

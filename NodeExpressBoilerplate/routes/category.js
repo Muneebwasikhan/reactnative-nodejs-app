c9f94c63d17838db288c9b1d7e9135f05921e8a8
NajamShehzad <najambutt195@gmail.com>
2019-02-23 16:23:41 +0500

    catergory get all + add

--- NodeExpressBoilerplate/routes/category.js
+++ NodeExpressBoilerplate/routes/category.js
@@ -2,7 +2,7 @@ exports = module.exports = function (app, mongoose) {
     var express = require("express")
     var router = express.Router()
 
-    router.post("/add", function (req, res, next) {
+    router.post("admin/category/add", function (req, res, next) {
         try {
             let category = new app.db.models.Category({
                 name: req.body.name
@@ -18,7 +18,23 @@ exports = module.exports = function (app, mongoose) {
                 message: err.message
             });
         }
-    })
+    });
 
-    app.use("/admin/category", router)
+    router.post("/category/getall", function (req, res, next) {
+        try {
+            let CategoryModel = app.db.models.Category;
+            let categoryArray = await CategoryModel.find({})
+            res.send({
+                success: true,
+                data: categoryArray
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                message: err.message
+            });
+        }
+    });
+
+    app.use("/", router)
 }

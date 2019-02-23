5e2beb6150a9b7205497707e4898431d12597089
NajamShehzad <najambutt195@gmail.com>
2019-02-23 12:59:59 +0500

    USerList for Admin

--- NodeExpressBoilerplate/routes/index.js
+++ NodeExpressBoilerplate/routes/index.js
@@ -4,4 +4,5 @@ exports = module.exports = function (app, mongoose) {
   require("./services")(app, mongoose)
   require("./chat")(app, mongoose)
   require("./adminAuth")(app, mongoose)
+  require("./adminUser")(app, mongoose)
 }

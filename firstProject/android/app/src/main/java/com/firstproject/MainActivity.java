4de04b8e4739e2c85a335ce2b1c41599acd15acf
muneebwasikhan <muneebwasikhan@gmail.com>
2019-03-01 12:56:24 +0500

    three page

--- firstProject/android/app/src/main/java/com/firstproject/MainActivity.java
+++ firstProject/android/app/src/main/java/com/firstproject/MainActivity.java
@@ -1,8 +1,6 @@
 package com.firstproject;
 import android.content.Intent;
 import com.facebook.react.ReactActivity;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-
 
 public class MainActivity extends ReactActivity {
 
@@ -15,13 +13,6 @@ public class MainActivity extends ReactActivity {
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-                new ReactNativeContacts());
-    }
-
     @Override
     protected String getMainComponentName() {
         return "firstProject";
--- firstProject/config/Path.js
+++ firstProject/config/Path.js
@@ -1,5 +1,5 @@
 export default class path {
-  static BASE_URL = 'http://localhost:3001';
+  static BASE_URL = 'https://61107208.ngrok.io';
 
   static LOGIN_AUTHENTICATION = path.BASE_URL + '/userauth';
 

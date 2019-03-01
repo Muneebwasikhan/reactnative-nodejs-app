c0bb99a741cebb6261ea60d582fc726b5f6c828a
muneebwasikhan <muneebwasikhan@gmail.com>
2019-03-01 20:31:58 +0500

    removed folder

--- NodeExpressBoilerplate/model/index.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function (app, mongoose) {
-  require("./user")(app, mongoose)
-  require("./services")(app, mongoose)
-  require("./chat")(app, mongoose)
-  require("./messages")(app, mongoose)
-  require("./category")(app, mongoose)
-}

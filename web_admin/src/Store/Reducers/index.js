d500bc7a241f6f96060361baaf94fbd47ec3cc10
muneebwasikhan <muneebwasikhan@gmail.com>
2019-03-02 15:00:49 +0500

    abcd

--- web_admin/src/Store/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import DataFetchReducer from "./DataFetchReducer/DataFetchReducer"
-
-
-const rootReducer = combineReducers({
-    DataFetchReducer
-})
-
-export default rootReducer;
